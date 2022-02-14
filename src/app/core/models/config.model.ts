de4fc6d1dceddb177f967cf19f1a201c8dd22d41
Juan Fajardo <juanfar.group@gmail.com>
2022-02-14 21:47:38 +0100

    Revert "feat: create movies, actors and companies services"

--- src/app/core/models/config.model.ts
+++
@@ -1,4 +0,0 @@
-export interface AppConfig {
-  env: string;
-  apiUrl: string;
-}
--- src/app/core/services/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './config/config.service';
-export * from './features/actors/actors.service';
-export * from './features/movies/movies.service';
-export * from './features/companies/companies.service';
