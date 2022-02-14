8d26fb5d7571ca6253dd0deb18fb575b803c4d2c
Juan Fajardo <juanfar.group@gmail.com>
2022-02-14 22:17:33 +0100

    feat: create movies, actors and companies services and config APP_INITIALIZER

---
+++ src/app/core/services/index.ts
@@ -0,0 +1,4 @@
+export * from './config/config.service';
+export * from './features/actors/actors.service';
+export * from './features/movies/movies.service';
+export * from './features/companies/companies.service';
